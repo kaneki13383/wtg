a989ef7e1b82fd01f4ce58bd46f0941b841fb8af
Daniil Kunitcyn <danchik.kun@mail.ru>
2022-06-08 22:06:49 +0300

    БД

--- wtg.sql
+++ wtg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 08 2022 г., 10:35
+-- Время создания: Июн 08 2022 г., 22:06
 -- Версия сервера: 8.0.24
 -- Версия PHP: 7.4.27
 
@@ -20,6 +20,70 @@ SET time_zone = "+00:00";
 --
 -- База данных: `wtg`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `excursion`
+--
+
+CREATE TABLE `excursion` (
+  `id` int NOT NULL,
+  `day` varchar(100) DEFAULT NULL,
+  `time` varchar(35) DEFAULT NULL,
+  `full_name` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `tel` varchar(50) DEFAULT NULL,
+  `default_bilet` int DEFAULT NULL,
+  `preferential_bilet` int DEFAULT NULL,
+  `summ` int DEFAULT NULL,
+  `id_user` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `excursion`
+--
+
+INSERT INTO `excursion` (`id`, `day`, `time`, `full_name`, `email`, `tel`, `default_bilet`, `preferential_bilet`, `summ`, `id_user`) VALUES
+(1, '6 июня', '9:00', 'Куницын Даниил Олегович', 'danchik.kun@mail.ru', '89673312786', 1, 2, 2800, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  `text` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `text`) VALUES
+(1, 'Даниил', 'danchik.kun@mail.ru', 'ewrgfergergerg erg erg rreg greqeg gwr qewg qewrg rgqeg qrewgqwerg qerw gerg eger e geg qeg ');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `subscribe`
+--
+
+CREATE TABLE `subscribe` (
+  `id` int NOT NULL,
+  `email` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `subscribe`
+--
+
+INSERT INTO `subscribe` (`id`, `email`) VALUES
+(4, 'kunitcyndaniil@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -41,11 +105,30 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `full_name`, `email`, `login`, `password`, `role`) VALUES
-(1, 'Иванов Иван Иванович', 'danchik.kun@mail.ru', 'kaneki13383', '123', 0);
+(1, 'Иванов Иван Иванович', 'danchik.kun@mail.ru', 'kaneki13383', '123', 1),
+(3, 'кенрекекнкенке', 'kunitcyndaniil@gmail.com', 'kog', '111', 0);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `excursion`
+--
+ALTER TABLE `excursion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `subscribe`
+--
+ALTER TABLE `subscribe`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `users`
@@ -58,10 +141,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `excursion`
+--
+ALTER TABLE `excursion`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `subscribe`
+--
+ALTER TABLE `subscribe`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
